4ab98decb6c3684ce98b8a98e2cb8dd88c2dd9ba
streetlightkids <ltj_hq@hotmail.com>
2021-12-30 08:35:12 -0800

    add password input show/hide functionality

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const showPassIcon = document.getElementById("showPass");
 const hidePassIcon = document.getElementById("hidePass");
+const passInput = document.getElementById("password");
 
 hidePassIcon.addEventListener("click", showPassword);
 
 function showPassword() {
         showPassIcon.style.visibility = "visible";
         hidePassIcon.style.visibility = "hidden";
+        passInput.type = "text";
+
 }
+
 
 showPassIcon.addEventListener("click", hidePassword);
 
 function hidePassword() {
         hidePassIcon.style.visibility = "visible";
         showPassIcon.style.visibility = "hidden";
+        passInput.type = "password";
+
 }
